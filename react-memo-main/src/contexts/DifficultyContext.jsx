ab315c118aa85a2b48ad126ad8f83a37972b5485
Сергей Сёмин <128413214+SteveO95@users.noreply.github.com>
2024-06-18 00:35:02 +0300

    hw-1.and.2

---
+++ react-memo-main/src/contexts/DifficultyContext.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+export const DifficultyContext = React.createContext(null);
+export const DifficultyProvider = ({ children }) => {
+  const [isEasy, setIsEasy] = useState(false);
+  return <DifficultyContext.Provider value={{ isEasy, setIsEasy }}>{children}</DifficultyContext.Provider>;
+};
